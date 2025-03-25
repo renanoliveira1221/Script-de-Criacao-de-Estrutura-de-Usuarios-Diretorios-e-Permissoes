2c397eba4aeb03c7ae22464122ba4cd74b2de218
Renan Melo de Oliveira <168653364+renanoliveira1221@users.noreply.github.com>
2025-03-25 15:51:46 -0300

    Update script.sh
    parametro -crypt removido

--- script.sh
+++ script.sh
@@ -20,16 +20,16 @@ chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
 
-useradd carlos -p '$(openssl passwd -crypt senha123)' -G GRP_ADM -s /bin/bash
-useradd maria -p '$(openssl passwd -crypt senha123)' -G GRP_ADM -s /bin/bash
-useradd joao -p '$(openssl passwd -crypt senha123)' -G GRP_ADM -s /bin/bash
+useradd carlos -p $(openssl passwd senha123) -G GRP_ADM -s /bin/bash -m
+useradd maria -p $(openssl passwd senha123) -G GRP_ADM -s /bin/bash -m
+useradd joao -p $(openssl passwd senha123) -G GRP_ADM -s /bin/bash -m
 
-useradd debora -p '$(openssl passwd -crypt senha123)' -G GRP_VEN -s /bin/bash
-useradd sebastiana -p '$(openssl passwd -crypt senha123)' -G GRP_VEN -s /bin/bash
-useradd roberto -p '$(openssl passwd -crypt senha123)' -G GRP_VEN -s /bin/bash
+useradd debora -p $(openssl passwd senha123) -G GRP_VEN -s /bin/bash -m
+useradd sebastiana -p $(openssl passwd senha123) -G GRP_VEN -s /bin/bash -m
+useradd roberto -p $(openssl passwd senha123) -G GRP_VEN -s /bin/bash -m
 
-useradd josefina -p '$(openssl passwd -crypt senha123)' -G GRP_SEC -s /bin/bash
-useradd amanda -p '$(openssl passwd -crypt senha123)' -G GRP_SEC -s /bin/bash
-useradd rogerio -p '$(openssl passwd -crypt senha123)' -G GRP_SEC -s /bin/bash
+useradd josefina -p $(openssl passwd senha123) -G GRP_SEC -s /bin/bash -m
+useradd amanda -p $(openssl passwd senha123) -G GRP_SEC -s /bin/bash -m
+useradd rogerio -p $(openssl passwd senha123) -G GRP_SEC -s /bin/bash -m
 
 echo 'Script finalizado'
